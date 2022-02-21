61a67406ffe81283fb2623de32ce43ebcb5a24d8
hardstein <89636138+hardstein@users.noreply.github.com>
2022-02-21 12:41:41 +0100

    Added HTML class

---
+++ website-template/src/com/company/HTML.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class HTML {
+
+    static void starterProject() {
+        System.out.println("Starter");
+    }
+
+    static void starterProject(String robots) {
+        System.out.println("Starter with robots.txt");
+    }
+
+    static void hero() {
+        System.out.println("Hero header");
+    }
+
+    static void hero(String text, String picturePath) {
+        System.out.println("Hero header with the text" + text + " and image from path.");
+    }
+}
--- website-template/src/com/company/Main.java
+++ website-template/src/com/company/Main.java
@@ -4,19 +4,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        // 
-        String foldername = "websiteTemplate";  // Project name
-        Folder folder = new Folder(foldername);
-        folder.createFolder();
-        HtmlTemplate index = new HtmlTemplate("./websiteTemplate/index.html");
-        DummyText dummyText = new DummyText(20);
-        String text = dummyText.createDummyText();
-        index.htmlCompleteWebsiteTemplate("styles.css", "Template", text);
-        CssTemplate styles = new CssTemplate("./websiteTemplate/styles.css");
-        styles.fillCssTemplate();
-        JavaScriptTemplate script = new JavaScriptTemplate("./websiteTemplate/script.js");
-        script.createBasicScriptFile();
-        
+        HTML.starterProject();
+        HTML.starterProject("robots");
     }
 }
