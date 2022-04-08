571c1241dc8af08c3b512e6f8c4a6967ea4728aa
Kristiankaspersen <kristian.kaspersen@hiof.no>
2022-04-08 23:31:31 +0200

    Added JavaDoc for Nav and Main

--- websiteGenerator/websiteGenerator/HTMLTags/Body/SemanticElements/Main.java
+++ websiteGenerator/websiteGenerator/HTMLTags/Body/SemanticElements/Main.java
@@ -2,24 +2,49 @@ package websiteGenerator.HTMLTags.Body.SemanticElements;
 
 import websiteGenerator.Util.GetHTMLTagContent;
 
+/**
+ * A class for semantic main tag
+ *
+ * @version 1.0.0
+ */
 public class Main implements GetHTMLTagContent {
 
     private final String mainHeading;
     private final String mainContent;
 
+    /**
+     * Getter for main heading
+     * @return heading for main tag
+     */
     public String getMainHeading() {
         return mainHeading;
     }
 
+    /**
+     * Getter for main content
+     *
+     * @return main content
+     */
     public String getMainContent() {
         return mainContent;
     }
+
+    /**
+     * private constructor for Main
+     *
+     * @param builder
+     */
 
     private Main(Builder builder) {
         this.mainHeading = builder.mainHeading;
         this.mainContent = builder.mainContent;
     }
 
+    /**
+     * Factory for building a main tag
+     *
+     * @return Builder
+     */
     public static Builder createElementBuilder() {
         return new Builder();
     }
@@ -43,20 +68,40 @@ public class Main implements GetHTMLTagContent {
         return content;
     }
 
+    /**
+     * Builder class for building a header {@link Main}
+     */
     public static class Builder {
         private String mainHeading;
         private String mainContent;
 
-        public Builder addMainHeading(final String mainHeading) {
+        /**
+         * Adds the main heading
+         *
+         * @param mainHeading
+         * @return main heading string text
+         */
+        public Builder addMainHeading(String mainHeading) {
             this.mainHeading = mainHeading;
             return this;
         }
 
-        public Builder addMainContent(final String mainContent) {
+        /**
+         * Adds the main content
+         *
+         * @param mainContent
+         * @return main content string text
+         */
+        public Builder addMainContent(String mainContent) {
             this.mainContent = mainContent;
             return this;
         }
 
+        /**
+         * Builds the complete Main object
+         *
+         * @return Main object
+         */
         public Main build() {
             return new Main(this);
         }
--- websiteGenerator/websiteGenerator/HTMLTags/Body/SemanticElements/Nav.java
+++ websiteGenerator/websiteGenerator/HTMLTags/Body/SemanticElements/Nav.java
@@ -2,24 +2,50 @@ package websiteGenerator.HTMLTags.Body.SemanticElements;
 
 import websiteGenerator.Util.GetHTMLTagContent;
 
+/**
+ * A class for semantic nav tag
+ *
+ * @version 1.0.0
+ */
 public class Nav implements GetHTMLTagContent {
 
     private final String[] pageLinks;
     private final String logoImage;
 
+    /**
+     * Getter for page links attribute, this is the navbar menu
+     *
+     * @return a list of page lings
+     */
     public String[] getAddPageLinks() {
         return pageLinks;
     }
 
+    /**
+     * Getter for image logo path
+     *
+     * @return path for image
+     */
     public String getAddLogoImage() {
         return logoImage;
     }
 
+    /**
+     * private constructor for Nav class
+     *
+     * @param builder
+     */
     private Nav(Builder builder) {
         this.pageLinks = builder.addPageLinks;
         this.logoImage = builder.addLogoImage;
     }
 
+
+    /**
+     * Factory for creating a nav object
+     *
+     * @return Builder
+     */
     public static Builder createElementBuilder() {
         return new Builder();
     }
@@ -53,20 +79,39 @@ public class Nav implements GetHTMLTagContent {
         return content;
     }
 
+    /**
+     * Builder class for building a header {@link Nav}
+     */
     public static class Builder {
         private String[] addPageLinks;
         private String addLogoImage;
 
+        /**
+         * Adds a list of page links for you navbar menu
+         * @param addPageLink
+         * @return list of pagelinks for menu
+         */
         public Builder addPageLinks(final String[] addPageLink) {
             this.addPageLinks = addPageLink;
             return this;
         }
 
+        /**
+         * Adds image logo path for picture in you navbar
+         *
+         * @param addLogoImage
+         * @return image path
+         */
         public Builder addLogoImage(final String addLogoImage) {
             this.addLogoImage = addLogoImage;
             return this;
         }
 
+        /**
+         * Makes the complete Nav object
+         *
+         * @return Nav object
+         */
         public Nav build() {
             return new Nav(this);
         }
