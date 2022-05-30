e3db086e762a5fd731c0944a4c49a459de608c9b
hardstein <89636138+hardstein@users.noreply.github.com>
2022-05-30 10:17:15 +0200

    Update WebsiteBuilder.java

--- websiteGenerator/websiteGenerator/WebsiteBuilder.java
+++ websiteGenerator/websiteGenerator/WebsiteBuilder.java
@@ -84,10 +84,10 @@ public class WebsiteBuilder {
                                 page.getHead().getContentOfElement() + "\n" +
                                 // Add Theme
                                 "<body class=\"container h-100 bg-" + theme.getThemeColor() +" text-" + theme.getFontColor() + "\">\n" +
-                                page.getNav().createNav(theme, pages) + "\n" +
+                                page.getNav().createNav(theme, pages) +
                                 page.getHeader().createHeader() +
                                 page.getMain().createMain() +
-                                page.getAside().getContentOfElement() + "\n" +
+                                page.getAside().createAside() +
                                 page.getFooter().createFooter(theme) +
                                 "    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2\" crossorigin=\"anonymous\"></script>\n" +
                                 "</body>\n" +
