21f961428e15bda81920ae3698bc0d5c6da538df
hardstein <89636138+hardstein@users.noreply.github.com>
2022-06-01 17:59:55 +0200

    Added javadoc

--- websiteGenerator/websiteGenerator/util/FontFamily.java
+++ websiteGenerator/websiteGenerator/util/FontFamily.java
@@ -1,19 +1,37 @@
 package websiteGenerator.util;
 
+/**
+ * Enum class for font family.
+ */
 public enum FontFamily {
+    /**
+     * Font family for Times New Roman.
+     */
     TimesNewRoman("times"),
+    /**
+     * Font family for Arial.
+     */
     ARIAL("arial");
 
     private String fontFamily;
 
+    /**
+     * Constructor for font family.
+     */
     FontFamily(String fontFamily) {
         this.fontFamily = fontFamily;
     }
 
+    /**
+     * Getter for font family.
+     */
     public String getFontFamily() {
         return fontFamily;
     }
 
+    /**
+     * Setter for font family.
+     */
     public void setFontFamily(String fontFamily) {
         this.fontFamily = fontFamily;
     }
