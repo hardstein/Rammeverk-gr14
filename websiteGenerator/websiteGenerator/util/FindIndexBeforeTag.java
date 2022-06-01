0efcc8583b5dc7a9eff99b6fb40295c3911f45c8
hardstein <89636138+hardstein@users.noreply.github.com>
2022-06-01 11:45:56 +0200

    Added javadoc

--- websiteGenerator/websiteGenerator/util/FindIndexBeforeTag.java
+++ websiteGenerator/websiteGenerator/util/FindIndexBeforeTag.java
@@ -1,6 +1,12 @@
 package websiteGenerator.util;
 
 public class FindIndexBeforeTag {
+    /**
+     * This method will find the index before a specified word in a StringBuilder.
+     * @param sb The StringBuilder to find an index.
+     * @param word The string to find the index in front of.
+     * @return int with the index before the word inside the StringBuilder.
+     */
     public static int findIndexBeforeTag(StringBuilder sb, String word) {
         return sb.indexOf(word);
     }
