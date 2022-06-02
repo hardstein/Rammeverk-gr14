d4be7a1fe8cdea7838f86b9f5aed569c5bc9b4b6
Kristiankaspersen <kristian.kaspersen@hiof.no>
2022-06-03 01:32:52 +0200

    Have made scenario 4 and 11, and cleaned the main where we test the scenarioes, also linked them to each scenario from the report. The output from the scenarios are ready for screenshots in to the report, now we are making four pages, Home, registration, login, aricle. Have also added the content for the registration page, it was missing

--- websiteGenerator/scenarioes/Main.java
+++ websiteGenerator/scenarioes/Main.java
@@ -25,12 +25,12 @@ public class Main {
 
         String heroImageUrl = "https://images.unsplash.com/photo-1534171472159-edb6d1e0b63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
         // Demonstration of our API (We will show a solution for all the scenarios here)
-        // For now you can find scenario: 1, 3, 7, 9, 14
+        // You can find scenario: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
 
         //Creating page content
-        //Head mainHead = Head.createHead("styles.css", "Frontpage");
-        // These will be the same for all pages:
-        // Scenario 7:
+
+        // These will be the same for all pages (footer, nav) :
+        // Scenario 7: (create a footer that includes -> contact info, copyright, social media)
         Footer footer = Footer.createElementBuilder()
 //                .addContactInformationContainer()
 //                .addSocialMediaLinksContainer()
@@ -42,34 +42,19 @@ public class Main {
                 .addFacebookSocialMediaLink("facebook link")
                 .build();
 
-        Footer articleFooter = Footer.createElementBuilder()
-//                .addSocialMediaLinksContainer()
-//                .addContactInformationContainer()
-                .addContactName("Per")
-                .addTwitterSocialMediaLink("Twitter link")
-                .addContactName("Pål")
-                .addContactPhoneNumber(32352345)
-                .addContactName("Per")
-                .addContactPhoneNumber(765476548)
-                .addTwitterSocialMediaLink("Twitter link")
-                .addFacebookSocialMediaLink("facebook link")
-                .addFacebookSocialMediaLink("facebook link")
-                .addContactName("Askeladden")
-                .build();
-
         Aside aside = Aside.createElementBuilder()
                 .addAsideTitle("Lorem ipsum")
                 .addAsideText("Aliquam elementum dolor porta ligula euismod venenatis. Curabitur sit amet malesuada elit. Proin ultrices id velit eu consectetur. Cras ex nunc, venenatis ut urna eu, maximus pulvinar ligula. Proin ut est metus. Curabitur et lorem sodales nunc lacinia blandit vitae efficitur leo. Nunc dapibus leo eget orci porta placerat. Phasellus efficitur, tortor ac rutrum fringilla, nisl ante fermentum est, eget congue massa augue at quam. Ut accumsan imperdiet felis dapibus sagittis. Morbi eget placerat leo, sed dapibus nisl. Maecenas at urna posuere, suscipit mauris in, aliquet diam. ")
                 .addAsideText("Fusce sed enim eu augue sagittis fringilla sit amet ac ante. Cras vitae vehicula nunc, at ullamcorper nibh. In nulla lectus, vehicula et eros id, pharetra porta libero. ")
                 .build();
 
-        // Scenario 3:
+        // Scenario 4:  (creating a logo on top of the webpage adapting to the screen)
         Nav nav = Nav.createElementBuilder()
                 .addLogoImage("https://images.unsplash.com/photo-1650447973536-f2c1019c62e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fDE0JTIwbnVtYmVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60")
                 .build();
 
-        // Testing form
-        Form loginForm = Form.createFormBuilder()
+        // Scenario 10 part 1 (Create a signup form, the form shall consist of input fields -> name, address, email, password)
+        Form registrationForm = Form.createFormBuilder()
                 .addTextLabel("Name")
                 .addTextField()
                 .addTextLabel("Address")
@@ -82,21 +67,43 @@ public class Main {
                 .addButtonLabel("Register")
                 .addSubmitButton()
                 .build();
+
+        // Scenario 11 Part 1 ( Create a login form, the form shall consist of username and password )
+        Form loginForm = Form.createFormBuilder()
+                .addTextLabel("username")
+                .addTextField()
+                .addTextLabel("password")
+                .addPasswordField()
+                .addButtonLabel("Login")
+                .addSubmitButton()
+                .build();
+
         // Different main content on pages:
-        // Article: 
+        // Article:
+        // Scenario 5 Part 1. (create an article page and populate it with text from a txt file)
         websiteGenerator.htmlTags.body.Main articleMain = websiteGenerator.htmlTags.body.Main.createElementBuilder()
                 .addMainHeading("Title in the article template")
                 .addIntroduction("Praesent placerat mattis purus id consectetur. Sed condimentum tempor mattis. Vestibulum convallis quis leo eu tincidunt. Nam in eros ac tortor ultricies rhoncus.")
                 .addFontFamily(TimesNewRoman)
+                .addTxtToHtml("Scenario5.txt")
                 .addConclusion("Duis hendrerit at augue et efficitur. Phasellus ut magna varius, rhoncus eros id, molestie lectus. Cras ultricies vestibulum accumsan. Nulla hendrerit justo metus, ut suscipit velit aliquam ut. ")
                 .build();
+
+        // Scenario 6 (Create a CSV file and populate it with text from a CSV file )
+        Table premiereLeagueTable = Table.createTableBuilder()
+                .addCSVToHTMLTable("planets_100.csv", ";")
+                .build();
+
+        System.out.println("This is the output for scenario 6:");
+        System.out.println(premiereLeagueTable.getTable());
 
         Header articleHeader = Header.createElementBuilder()
                 .addHeaderHeading("ARTICLE ER NICE")
                 .addHeaderImageUrl("background.png")
                 .build();
 
-        // Home: 
+        // Home:
+        // Scenario 1 (Adding hero)
         Header homeHeader = Header.createElementBuilder()
                 .addHeroHeading("Hero-Title")
                 .addHeroImageUrl(heroImageUrl)
@@ -104,6 +111,8 @@ public class Main {
                 .createHeroHeader()
                 .build();
 
+        // Scenario 2 ( choose text size and weight )
+        // Scenario 12 ( Add functionality for changing the font family in the article)
         websiteGenerator.htmlTags.body.Main homeMain = websiteGenerator.htmlTags.body.Main.createElementBuilder()
                 .addMainHeading("Main Title")
                 .addIntroduction("The homepage is nice")
@@ -113,14 +122,19 @@ public class Main {
                 .addFontFamily(ARIAL)
                 .build();
 
-        // Creating pages
         Header loginHeader = Header.createElementBuilder().build();
 
-        // Only using default values
+        // Scenario 4 (adding main tag with txtToHTML)
         Article article = Article.createArticlePageBuilder()
+                .addMain(articleMain)
+                .addNav(nav)
+                .addHeader(articleHeader)
+                .addAside(aside)
+                .addFooter(footer)
                 .build();
 
-        // remember to add head aswell, removed it because of temporary faults
+        // remember to add head as well, removed it because of temporary faults (hva er dette?)
+        // Scenario 8 (Create a HTML page that adapts to the screen size)
         Home homePage = Home.createPageBuilder()
                 .addFileName("homepage.html")
                 .addHead(Head.createHeadBuilder()
@@ -133,11 +147,18 @@ public class Main {
                 .addFooter(footer)
                 .build();
 
+        // Scenario 10 part 2 ( create a main section with a signup up user form )
+        websiteGenerator.htmlTags.body.Main registrationMain = websiteGenerator.htmlTags.body.Main.createElementBuilder()
+                .addMainHeading("Sign up")
+                .addForm(registrationForm)
+                .build();
+
+        // Scenario 11 part 2 ( create a main section with a login form )
         websiteGenerator.htmlTags.body.Main loginMain = websiteGenerator.htmlTags.body.Main.createElementBuilder()
                 .addMainHeading("Login")
                 .addForm(loginForm)
                 .build();
-                
+
         Login loginPage = Login.createPageBuilder()
                 .addMain(loginMain)
                 .addHead(Head.createHeadBuilder()
@@ -149,23 +170,35 @@ public class Main {
                 .addFooter(footer)
                 .build();
 
+        Registration registrationPage = Registration.createPageBuilder()
+                .addMain(registrationMain)
+                .addHead(Head.createHeadBuilder()
+                        .addTitle("Registration")
+                        .addStyle("styles")
+                        .build())
+                .addNav(nav)
+                .addHeader(loginHeader)
+                .addFooter(footer)
+                .build();
+
+
+
         Styles styles = Styles.createStylesBuilder()
                 .addFilename("styles.css")
                 .build();
         // Making project:
-        // Scenario 1, 9 and 14:
-        Page[] HTMLpages = new Page[]{homePage, article, loginPage};
+        // Scenario 3 (Generation a project folder with CSS and HTML files)
+        // Scenario 9 (Style the built pages in the project with a specific design theme
+        // Scenario 13 (Add robots.txt file to the project )
+        Page[] HTMLpages = new Page[]{homePage, article, loginPage, registrationPage};
         WebsiteBuilder project = WebsiteBuilder.createWebsiteBuilder()
                 .addProjectFolder("testFolder")
-                .addTheme(Theme.LIGHT)
+                .addTheme(Theme.DARK)
                 .addHTMLPages(HTMLpages)
                 .addRobots()
                 .addStylesheetFile(styles)
                 .build();
 
-//        project.generate();
-
-        System.out.println();
     }
 
 }
--- websiteGenerator/websiteGenerator/util/Parse.java
+++ websiteGenerator/websiteGenerator/util/Parse.java
@@ -93,7 +93,7 @@ public abstract class Parse implements Parsers {
             }
 
             htmlTxt.append("</p>").append("\n");
-            htmlTxt.append("</section>");
+            htmlTxt.append("</section>").append("\n");
 
         }
         catch (IOException e) {
